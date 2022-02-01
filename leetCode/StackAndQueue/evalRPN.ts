28a86ce3da1f02202ec5ab40f59653e6935242c2
Ming Ye/WZS/Wistron <Ming_Ye@wistron.com>
2022-02-01 23:13:05 +0800

    添加一道中缀表达式转后缀表达式，待调试

--- leetCode/StackAndQueue/evalRPN.ts
+++ leetCode/StackAndQueue/evalRPN.ts
@@ -15,7 +15,7 @@ function evalRPN(tokens: string[]): number {
         if (token == '+' || token == '-' || token == '*' || token == '/') {
             computeAndpushNumber(stack, token);
         } else {
-        // 否则直接推入
+            // 否则直接推入
             stack.push(+token);
         }
 
@@ -54,4 +54,79 @@ function computeAndpushNumber(
     }
 }
 
-console.log(evalRPN(["4", "13", "5", "/", "+"]))
+// console.log(evalRPN(["4", "13", "5", "/", "+"]));
+
+
+
+
+// 加餐：   中缀表达式转后缀表达式  
+function transfer(tokens: string[]) {
+
+    // 如 ((a+b)*c)+d*e == > ab+c*de*+  
+
+    const operatorsStack: string[] = [];
+    const result: string[] = [];
+    for (const token of tokens) {
+        console.log("operatorsStack:",operatorsStack)
+        console.log("result:",result)
+        if (token != "+"
+            && token != "-"
+            && token != "*"
+            && token != "/"
+            && token != "("
+            && token != ")"
+
+        ) {
+            // 数字或者( 就推入 
+            result.push(token);
+        } else if (token == "(") { 
+            operatorsStack.push(token);
+        }
+        else if (token == ")") {
+
+            let topValue = operatorsStack.pop() as string;
+            // 循环拿出 ( 前面的 运算符
+            while (topValue != "(") {
+                result.push(topValue);
+                topValue = operatorsStack.pop() as string;
+            }
+
+        } else {
+            // token 只剩下 +、-、*、/          
+            // 运算符的那么做一个比较优先级
+            if (operatorsStack.length == 0) {
+                operatorsStack.push(token);
+            } else {
+
+                let topValue = operatorsStack[operatorsStack.length - 1];
+                if (
+                     (topValue == "+" || topValue == "-" )
+                     && 
+                     (token == "*" || token == "/") ) {
+                    // 这两个优先级都挺高的 
+                    operatorsStack.push(token);
+                } else if (topValue == "(") {  
+                    // 假设这个为 ( 那么我们直接推入运算符
+                    operatorsStack.push(token);
+                }else{
+                    topValue = operatorsStack.pop() as string;
+                    result.push(operatorsStack.pop() as string );
+                    operatorsStack.push(token);
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+    return result;
+
+
+
+}
+
+console.log(transfer("2*(9+6/3-5)+4".split("")))
