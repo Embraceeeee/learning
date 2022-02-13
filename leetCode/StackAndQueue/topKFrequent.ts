e8baa11028f5c5ac86ea03fa3ab6813a86167707
Embraceeeee <980080797@qq.com>
2022-02-13 21:10:45 +0800

    finish #347

--- leetCode/StackAndQueue/topKFrequent.ts
+++ leetCode/StackAndQueue/topKFrequent.ts
@@ -1,19 +1,13 @@
-function topKFrequent(nums: number[], k: number): number[] {
-
-    return [];
-
-};
-
-
 // 比较函数的类型 接口
 interface ComparedFunction {
-    (a: [number, number], b: [number, number]): boolean
+    (a: [number, number], b: [number, number]): number
 }
 class PriorityQueue {
 
 
-    queue: [number, number][] = [];
-    compareFN: ComparedFunction = () => false;
+    private queue: [number, number][] = [];
+    private compareFN: ComparedFunction = () => 0;
+
     constructor(
         comparedFn: ComparedFunction
     ) {
@@ -21,9 +15,9 @@ class PriorityQueue {
     }
 
 
-    public push(item: [number, number]) {
+    public push(entry: [number, number]) {
 
-        this.queue.push(item);
+        this.queue.push(entry);
 
         let index = this.queue.length - 1;
         // 找刚插入的点的父节点
@@ -45,7 +39,7 @@ class PriorityQueue {
     public pop() {
 
         const ret = this.queue[0];
-        // 把最后一个节点移到 堆顶  
+        // 把最后一个节点移到 堆顶  (其实移除的也是顶点，最小值)
         this.queue[0] = this.queue.pop() as [number, number];
 
         let index = 0;
@@ -53,7 +47,7 @@ class PriorityQueue {
         let left = 1;
         // 找到第二层哪个元素是比较小的
         let selectedChild = this.compare(left, left + 1) > 0 ? left + 1 : left;
-        // 下沉
+        // 下沉 （大元素往下沉）
         while (selectedChild !== undefined && this.compare(index, selectedChild) > 0) {
 
             //  index是父节点， selectedChild 为子节点中较小的 
@@ -69,7 +63,7 @@ class PriorityQueue {
         return ret;
     }
 
-    public size() { 
+    public size() {
         return this.queue.length;
     }
 
@@ -99,3 +93,46 @@ class PriorityQueue {
 
 
 }
+
+function topKFrequent(nums: number[], k: number): number[] {
+
+
+    const result: number[] = [];
+    // 查看出现频率 
+    const map = new Map<number, number>();
+
+    for (const num of nums) {
+
+        const freq: undefined | number = map.get(num);
+        freq === undefined ? map.set(num, 1) : map.set(num, freq + 1);
+    }
+    console.log(map)
+    const priorityQueue: PriorityQueue = new PriorityQueue(
+        // 按照频率排序
+        (a: [number, number], b: [number, number]) => a[1] - b[1]
+    );
+    for (const entry of map.entries()) {
+        priorityQueue.push(entry);
+        if (priorityQueue.size() > k) {
+            priorityQueue.pop();
+        }
+    }
+
+
+    // 取出前 k 个元素 
+    // for (let i = 0; i < priorityQueue.size(); i++) {
+    //     result.push(priorityQueue.pop()[0]);
+    // }
+    // 上面的priorityQueue.pop() 会影响size()
+    const size = priorityQueue.size();
+    
+    for (let i = 0; i < size; i++) {
+        result.push(priorityQueue.pop()[0]);
+    }
+    return result;
+
+};
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+
+
