0d289a7c5dc4acde3a5694c7d60e0084f1c23bb3
Ming Ye/WZS/Wistron <Ming_Ye@wistron.com>
2022-02-19 17:37:50 +0800

    add solution backtracking in 二叉树的所有路径（257）

--- leetCode/BinaryTree/binaryTreePaths.ts
+++ leetCode/BinaryTree/binaryTreePaths.ts
@@ -20,7 +20,7 @@ function getPath(node: TreeNode | null, path: string, paths: string[]): string {
 
     // 空节点直接  return 
     if (node == null) {
-       // paths.push(path);
+        // paths.push(path);
         return path;
     }
 
@@ -31,8 +31,8 @@ function getPath(node: TreeNode | null, path: string, paths: string[]): string {
         path += node.val.toString();
     }
 
-     // 判断是否为叶子节点，若系就推入 paths  并return
-     if (node.left == null && node.right == null ) { 
+    // 判断是否为叶子节点，若系就推入 paths  并return
+    if (node.left == null && node.right == null) {
         paths.push(path);
         return path;
     }
@@ -58,4 +58,51 @@ var test1 = new TreeNode(1,
 
 
 
-console.log(binaryTreePaths(test1));
+console.log(binaryTreePaths_solution(test1));
+
+
+function binaryTreePaths_solution(root: TreeNode | null): string[] {
+
+    let paths: string[] = [];
+    let path: number[] = [];
+    if (root !=null ) {
+        traversal(root, path, paths);
+    }
+    return paths;
+
+};
+
+
+
+function traversal(node: TreeNode, path: number[], paths: string[]): void {
+
+
+    path.push(node.val);
+
+    // 判断是否为叶子节点，若系就推入 paths  并return
+    if (node.left == null && node.right == null) {
+        paths.push(getRequiredPath(path));
+        return;
+    }
+
+    // 该节点不是叶子节点，就继续遍历它的不为null的左右子节点   
+    if (node.left != null) {
+        traversal(node.left, path, paths);
+        path.pop();
+    }
+
+    if (node.right != null) {
+        traversal(node.right, path, paths);
+        path.pop();
+    }
+
+    return;
+
+}
+
+
+function getRequiredPath(path: number[]) {
+
+    return path.join("->");
+
+}
