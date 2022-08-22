d9e2ef2f6b09111a90bcee67c6da736f7da1665c
Embraceeeee <980080797@qq.com>
2022-08-22 19:55:47 +0800

    chore: update length of string title

--- type-challenges/length-of-string.ts
+++ type-challenges/length-of-string.ts
@@ -1,21 +1,33 @@
 /**
- *  计算字符串的长度，类似于 String#length 。 
+ *  计算字符串的长度，类似于 String#length 。
  */
-
 
 // type LengthOfString<T extends string > = T['length'];
 
-
 // type test = LengthOfString<'AAA'>;
-
 
 // 闪过一个念头：把字符串转成数组，然后用数组的length可以拿到长度
 // 看了一眼答案发现真的如此
 
-// TODO: 待调整
+type ToArray<T extends string> = T extends `${infer F}${infer R}`
+  ? [F, ...ToArray<R>]
+  : [];
 
-type ToArray<T extends string> =  T extends `${infer F}${infer R}` ? [F,...LengthOfString<R>] : [];
+type LengthOfString<T> = T extends string ? ToArray<T>["length"] : 0;
 
-type LengthOfString<T> = T extends string ? ToArray<T>['length']:0;
+type length = LengthOfString<"AAA">;
 
-type length = LengthOfString<'ab'>;
+// 解答是这样写的：
+type LengthOfString1<
+  T extends string,
+  A extends string[] = []
+> = T extends `${infer F}${infer R}`
+  ? LengthOfString1<R, [F, ...A]>
+  : A["length"];
+
+
+type length1 = LengthOfString1<"AAACA">;
+
+// 递归到最深处拿值，最后返回一个数值，结束就是T不再是一个有F和R组成的字符串即为空串，然后返回A的长度
+
+
