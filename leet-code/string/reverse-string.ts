5c13ab3c8cd2f0ad81859888fa2634385a0828ce
Embraceeeee <980080797@qq.com>
2023-02-18 18:40:46 +0800

    chore: update frontend mini project(3) and add reverse string title

---
+++ leet-code/string/reverse-string.ts
@@ -0,0 +1,9 @@
+/**
+ *  @see https://programmercarl.com/0344.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2.html   
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  for (let i = 0; i < s.length / 2; i++) {
+    [s[i], s[s.length - 1 - i]] = [s[s.length - 1 - i], s[i]];
+  }
+}
